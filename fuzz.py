930ba482eb4e65c0bdecbfc118c2225a30c07545
Syed Faraz Abrar <faraz@elttam.com>
2020-06-02 06:36:43 -0700

    Added in some more callback methods. Refined the grammar for wrappers, interactions, and for loops. More to be done

--- fuzz.py
+++ fuzz.py
@@ -25,23 +25,33 @@ def main():
     # Used to name crash and timeout test cases
     crashes = timeouts = num = 0
 
-    for f in file_list:
-        num += 1
-        filename = f"./{sys.argv[1]}/{f}"
-        print(f"\r######## Test case {num} - {f} ########", end="", flush=True)
-        try:
-            child = subprocess.run(["./hermes/build_asan/bin/hermes", filename], timeout=TIMEOUT)
-            exit_code = child.returncode
-            if exit_code != 0:
-                crashes += 1
-                crash_file = f"crash{crashes}.js"
-                print(f"\nCrashed. Saved as {crash_file}")
-                shutil.copyfile(filename, f"./crashes/n{node_num}_{crash_file}.js")
-        except subprocess.TimeoutExpired:
-            timeouts += 1
-            crash_file = f"timeout{timeouts}.js"
-            print(f"\nTimed out. Saved as {crash_file}")
-            shutil.copyfile(filename, f"./timeouts/n{node_num}_{crash_file}")
+    # ASAN_OPTIONS environment variable
+    asan_options = "detect_leaks=0,exitcode=42,abort_on_error=1,disable_coredump=0"
+    env = os.environ.copy()
+    env["ASAN_OPTIONS"] = asan_options
+
+    try:
+        for f in file_list:
+            num += 1
+            filename = f"./{sys.argv[1]}/{f}"
+            print(f"\r######## Test case {num} - {f} ########", end="", flush=True)
+            try:
+                child = subprocess.run(["./hermes/build_asan/bin/hermes", filename], timeout=TIMEOUT,
+                        env=env)
+                exit_code = child.returncode
+                if exit_code != 0:
+                    crashes += 1
+                    crash_file = f"crash{crashes}.js"
+                    print(f"\nCrashed. Saved as {crash_file}")
+                    shutil.copyfile(filename, f"./crashes/n{node_num}_{crash_file}.js")
+            except subprocess.TimeoutExpired:
+                timeouts += 1
+    except KeyboardInterrupt:
+        percentage_timeout = float(timeouts) / float(num) * 100.0
+        with open(f"logs/fuzzer{node_num}", "w") as logfile:
+            logfile.write(
+                    f"Fuzzer {node_num} - Percentage of test cases that timed out: {percentage_timeout}%\n")
+        sys.exit(0)
             
 
 if __name__ == "__main__":
