b8fb7ed5504d63547c372ce08af2fb1954c815a9
Syed Faraz Abrar <faraz@elttam.com>
2020-06-05 11:05:02 -0700

    Modified generate.sh to use a max recursion limit of 3. Added some Object static methods and for...of loops to the grammar.

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 rm -rf inputs*
 mkdir inputs{1,2,3,4}
 
-screen -S generator1 -dm dharma -grammar grammar/js.dg -storage inputs1/ -format js -count 250000 &
-screen -S generator2 -dm dharma -grammar grammar/js.dg -storage inputs2/ -format js -count 250000 &
-screen -S generator3 -dm dharma -grammar grammar/js.dg -storage inputs3/ -format js -count 250000 &
-screen -S generator4 -dm dharma -grammar grammar/js.dg -storage inputs4/ -format js -count 250000 &
+screen -S generator1 -dm dharma -grammar grammar/js.dg -storage inputs1/ -format js -count 250000 -recursion-limit 3 &
+screen -S generator2 -dm dharma -grammar grammar/js.dg -storage inputs2/ -format js -count 250000 -recursion-limit 3 &
+screen -S generator3 -dm dharma -grammar grammar/js.dg -storage inputs3/ -format js -count 250000 -recursion-limit 3 &
+screen -S generator4 -dm dharma -grammar grammar/js.dg -storage inputs4/ -format js -count 250000 -recursion-limit 3 &
